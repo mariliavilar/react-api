7991646b54e97d3d2ff46c1c8ea121a862577d60
Marilia Vilar <mariliavilarpr@gmail.com>
2021-05-17 21:47:47 -0300

    listagem dos locatarios com nome fantasia e responsavel - integracao front porta 3000 com back na porta 8080

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    locatarios: [],
+  };
+
+  //usar componentDidMount para buscar os dados na api
+  //ele é  chamado imediatamente apos alguma atualizacao ocorrer
+  async componentDidMount() {
+    //const response = await api.get('/star%20wars'); //oq mais eu quiser add a url
+    const response = await api.get('');
+
+    console.log(response.data);
+
+    //setar os valores que receber
+    this.setState({ locatarios: response.data });
+  }
+
+  render() {
+    //buscar os dados que estao no state [apenas filmes]
+    const { locatarios } = this.state;
+
+    return (
+      <div>
+        <h1>Listar Locatarios</h1>
+
+        {console.log(locatarios)}
+        {locatarios.map((locatario) => (
+          <ul key={locatario.cnpj}>
+            <h2>
+              <strong>Empresa: </strong>
+              {locatario.nomeFantasia}
+            </h2>
+            <p>Responsável: {locatario.nomeResponsavel}</p>
+          </ul>
+        ))}
+
+        {/* vou percorrer o array filmes, utilizando o map. e ao percorrer cada filme, sera atribuido na posicao filme */}
+      </div>
+    );
+  }
 }
 
 export default App;
